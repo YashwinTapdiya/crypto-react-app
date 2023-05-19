1208d348df420db78a6e0dabff7bfdd41b14a722
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-19 09:46:12 +0530

    added exchangeCard

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect ,useState } from 'react';
 import axios from 'axios';
 import { server } from '../index';
-import { Container , HStack } from '@chakra-ui/react';
+import {
+    Container,
+    Heading,
+    HStack,
+    Image,
+    Text,
+    VStack,
+  } from "@chakra-ui/react";
 import Loader from './Loader';
 
 const Exchanges = () => {
@@ -31,7 +38,12 @@ const Exchanges = () => {
         <HStack wrap={"wrap"}> 
             {
                 exchanges.map((i)=>(
-                    <ExhangeCard />
+                    <ExchangeCard 
+                    key={i.id}
+                    name={i.name} 
+                    img={i.image} 
+                    rank={i.trust_score_rank} 
+                    url={i.url} />
                 ))
 
             }
@@ -42,8 +54,35 @@ const Exchanges = () => {
   )
 }
 
-const ExhangeCard=()=>{
+const ExchangeCard=( {name,img,rank,url} )=>(
+    <a href={url} target={"blank"}>
+<VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        objectFit={"contain"}
+        alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
 
-}
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+    </a>
+)
 
 export default Exchanges
