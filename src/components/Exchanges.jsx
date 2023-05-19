4111af08bd736320a9a6eed88f489710aeedae2a
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-19 09:34:09 +0530

    updated exchange page and added loader tag

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect ,useState } from 'react';
+import axios from 'axios';
+import { server } from '../index';
+import { Container , HStack } from '@chakra-ui/react';
+import Loader from './Loader';
 
 const Exchanges = () => {
+
+    const [exchanges , setExchanges] = useState([]);
+    const [loading , setLoading] =useState(true);
+
+//useEffect(effect: React.EffectCallback, deps?: React.DependencyList | undefined): void
+//If present, effect will only activate if the values in the list change.
+//Accepts a function that contains imperative, possibly effectful code.
+    useEffect(() =>{
+        const fetchExchanges = async() =>{
+            const {data} = await axios.get(`${server}/exchanges`)
+
+            console.log(data);
+            setExchanges(data);
+            setLoading(false);
+        };
+        fetchExchanges();
+    }, []);
+
+
   return (
-    <div>Exchanges</div>
+    <Container maxW={"container.xl"} >
+        {loading? <Loader /> : <>
+
+        <HStack wrap={"wrap"}> 
+            {
+                exchanges.map((i)=>(
+                    <ExhangeCard />
+                ))
+
+            }
+        </HStack>
+
+         </>}
+    </Container>
   )
+}
+
+const ExhangeCard=()=>{
+
 }
 
 export default Exchanges
